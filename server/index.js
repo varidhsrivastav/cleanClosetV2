aac18286aff48db912ff6485e73e0892bd64f713
Bishal Kundu <bishalkundu17@gmail.com>
2023-07-22 17:00:44 +0530

    Added Donor Registration API

---
+++ server/index.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+
+const Donor = require('./models/donor')
+
+main().catch(err => console.log(err))
+async function main() {
+    await mongoose.connect(process.env.MONGO_URL)
+}
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+app.post('/donorRegister', (req, res) => {
+    const data = req.body
+    const newDonor = new Donor(data)
+    newDonor.save()
+    res.status(200).json(newDonor)
+})
+
+app.listen(8080, () => {
+    console.log("Server started at 8080 port!")
+})
